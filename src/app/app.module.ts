a1070b0e3be3082431b237c2526217e86866fd65
Pedro <pedrolcrg@gmail.com>
2024-11-05 20:55:23 -0300

    Iniciando a tela de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { LoginScreenComponent } from './components/login-screen/login-screen.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    LoginScreenComponent
   ],
   imports: [
     BrowserModule
