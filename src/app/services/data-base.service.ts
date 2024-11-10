43e112261b9b64e21f4c0107b15b2e1db5844e9f
Odair Michael Bendotti <odairmichaelbendotti@gmail.com>
2024-11-10 10:07:54 -0300

    feat: Add interface user taks

--- src/app/services/data-base.service.ts
+++ src/app/services/data-base.service.ts
@@ -13,8 +13,16 @@ export class DataBaseService {
   }
 
   users: User[] = [
-    { id: 1, name: 'Pedro', email: 'pedro@teste.com', password: 'senha123' },
-    { id: 2, name: 'Odair', email: 'odair@teste.com', password: 'senha123' },
+    { id: 1, name: 'Pedro', email: 'pedro@teste.com', password: 'senha123',
+      tasks: {
+        importante: ["Finalizar projeto"],
+        trabalho: ["Reunião com cliente"],
+        estudos: ["Estudar TypeScript"],
+        lazer: ["Assistir série"],
+        compras: ["Comprar café", "Leite"]
+      }
+     },
+    // { id: 2, name: 'Odair', email: 'odair@teste.com', password: 'senha123' },
   ]
 
   userLogged: User | null = null; // Usuário logado atualmente
